b59418dbecfaace9882173e5ec8e2b00464c7597
Aleksander Pohl <apohllo@o2.pl>
2011-06-28 14:37:41 +0200

    - Constantize encoding names

--- lib/string_case_pl.rb
+++ lib/string_case_pl.rb
@@ -1,15 +1,18 @@
 # -*- coding: utf-8 -*-
 
 class String
+  UTF_8_ENCODING = "UTF-8"
+  ISO_8859_2_ENCODING = "ISO-8859-2"
+  WINDOWS_CP1250_ENCODING = "Windows-1250"
   # old up/down-case without Polish transcodings
   PL_UTF_8_LOWER = ("\xc4\x85\xc5\xbc\xc5\x9b\xc5\xba\xc4\x99" +
-    "\xc4\x87\xc5\x84\xc3\xb3\xc5\x82").force_encoding("UTF-8")
+    "\xc4\x87\xc5\x84\xc3\xb3\xc5\x82").force_encoding(UTF_8_ENCODING)
   PL_UTF_8_UPPER = ("\xc4\x84\xc5\xbb\xc5\x9a\xc5\xb9\xc4\x98" +
-    "\xc4\x86\xc5\x83\xc3\x93\xc5\x81").force_encoding("UTF-8")
-  PL_ISO_8859_2_LOWER = PL_UTF_8_LOWER.encode("ISO-8859-2")
-  PL_ISO_8859_2_UPPER = PL_UTF_8_UPPER.encode("ISO-8859-2")
-  PL_WINDOWS_1250_LOWER = PL_UTF_8_LOWER.encode("Windows-1250")
-  PL_WINDOWS_1250_UPPER = PL_UTF_8_UPPER.encode("Windows-1250")
+    "\xc4\x86\xc5\x83\xc3\x93\xc5\x81").force_encoding(UTF_8_ENCODING)
+  PL_ISO_8859_2_LOWER = PL_UTF_8_LOWER.encode(ISO_8859_2_ENCODING)
+  PL_ISO_8859_2_UPPER = PL_UTF_8_UPPER.encode(ISO_8859_2_ENCODING)
+  PL_WINDOWS_1250_LOWER = PL_UTF_8_LOWER.encode(WINDOWS_CP1250_ENCODING)
+  PL_WINDOWS_1250_UPPER = PL_UTF_8_UPPER.encode(WINDOWS_CP1250_ENCODING)
 
   alias old_downcase_wo_pl downcase
   alias old_upcase_wo_pl upcase
@@ -17,11 +20,11 @@ class String
 
   def downcase
     case self.encoding.name
-    when "UTF-8"
+    when UTF_8_ENCODING
       self.tr!(PL_UTF_8_UPPER,PL_UTF_8_LOWER)
-    when "ISO-8859-2"
+    when ISO_8859_2_ENCODING
       self.tr!(PL_ISO_8859_2_UPPER, PL_ISO_8859_2_LOWER)
-    when "Windows-1250"
+    when WINDOWS_CP1250_ENCODING
       self.tr!(PL_WINDOWS_1250_UPPER, PL_WINDOWS_1250_LOWER)
     end
     self.old_downcase_wo_pl
@@ -29,11 +32,11 @@ class String
 
   def upcase
     case self.encoding.name
-      when "UTF-8"
+    when UTF_8_ENCODING
       self.tr!(PL_UTF_8_LOWER, PL_UTF_8_UPPER)
-      when "ISO-8859-2"
+    when ISO_8859_2_ENCODING
       self.tr!(PL_ISO_8859_2_LOWER, PL_ISO_8859_2_UPPER)
-      when "Windows-1250"
+    when WINDOWS_CP1250_ENCODING
       self.tr!(PL_WINDOWS_1250_LOWER, PL_WINDOWS_1250_UPPER)
     end
     self.old_upcase_wo_pl
